e1ff7607393a6ddff0fe65bfc5c564b0b2890104
tiagogomesgh <tiagogomesfl@outlook.com>
2023-01-26 14:42:48 -0800

    Completed Exercise. Struggled with tertiary operator.

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            var answer = 4;
+            string response =
+                (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
